6a50c116ec4f367fc699c32010f8b9cd650514a1
darshan <darshanchauhan5064@gmail.com>
2023-12-14 09:11:03 +0530

    14 dec

---
+++ 11.js
@@ -0,0 +1,33 @@
+// array method
+
+let a= [11,22,33,44,55];
+console.log(a);
+console.log(data.length);
+
+// push -> insert element in last position
+// data.push(66);
+// data.push(77);
+// data.push(88);
+// console.log(a);
+// console.log(a.legth);
+
+// pop-> delete element in last position
+// data.pop();
+// data.pop();
+// console.log(a);
+
+// unshift -> insert element imn start position
+// data.unshift();
+// data.unshift();
+// console.log(a);
+
+// shift -> delete element in fisrt position 
+// data.shift();
+// data.shift();
+// console.log(a);
+
+// splice -> (start element, number element,new add)
+// data.splice(4,4);
+// data.splice(5,6,44);
+// console.log(a);
+  
