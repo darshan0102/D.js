e8bd4590cd107a2f36bd93f0da781cce3c8f1187
darshan <darshanchauhan5064@gmail.com>
2023-12-22 14:31:27 +0530

    22 dec

--- 18.js
+++ 18.js
@@ -1,24 +1,65 @@
-// javascript HTML DOM Events 
+/*  JavaScript HTML DOM Events */
 
 function message(){
-    alert("Yes, i am Heading Tag");
+    alert("Yes, I Am Heading Tag")
 }
 
-function datashow(){
-    document.getElemnetById("date").innerHTML=Date();
+function dateshow(){
+    document.getElementById("date").innerHTML = Date();
 }
 
-function UpperCase(){
-    const change = document.getElementById("emails");
+function uppercase(){
+    const change = document.getElementById("emails"); 
     change.value = change.value.toUpperCase();
 }
 
 function red(one){
-    one.style.colour = "red";
+    one.style.color= "red";
 }
 {
-    function Down(obj){
-        obj.style.backgroundcolor = "#1ec5e5";
-        obj.innerHTML =  "Release Me"
-    }
+    function Down(obj) {
+        obj.style.backgroundColor = "#1ec5e5";
+        obj.innerHTML = "Release Me";
+      }
+      
+      function Up(obj) {
+        obj.style.backgroundColor="#D94A38";
+        obj.innerHTML="Thank You";
+      }
 }
+
+/* JavaScript addEventListener() */
+
+{
+    const x = document.getElementById("clickIt");
+        const y = document.getElementById("hoverPara");
+          
+        x.addEventListener("click", RespondClick);
+        y.addEventListener("mouseover", RespondMouseOver);
+        y.addEventListener("mouseout", RespondMouseOut);
+          
+        function RespondMouseOver() {
+            document.getElementById("effect").innerHTML +=
+                       "MouseOver Event" + "<br>";
+        }
+          
+        function RespondMouseOut() {
+            document.getElementById("effect").innerHTML +=
+                      "MouseOut Event" + "<br>";
+        }
+          
+        function RespondClick() {
+            document.getElementById("effect").innerHTML +=
+                      "Click Event" + "<br>";
+        }
+}
+
+/*
+    1. calculator
+    2. digital clock / counter
+    3. word length counter
+    4. vowel character counter
+    5. random string generator
+    6. to-do list
+    7. budget app 
+*/
